e524626db059cc3e12c9f5b9021821598d3ff7c6
Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2020-04-11 18:42:26 +0300

    forgot to save

--- imago_project/imago/urls.py
+++ imago_project/imago/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path('members/', views.MembersView.as_view(), name='members'),
     path('members/<slug:slug>', views.MemberDetailView.as_view(), name='member_detail'),
     path('plays/', views.PlaysView.as_view(), name='plays'),
-    path('plays/<slug:slug>', views.PlayDetailView.as_view(), name='play_detail'),
-    path('signup/', views.SignUpView.as_view(), name='signup')
+    path('plays/<slug:slug>', views.PlayDetailView.as_view(), name='play_detail')
 ]
