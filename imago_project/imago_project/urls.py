d32ff3ab826278c0c3ac8705facc9ec66938310d
Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2020-04-17 22:56:54 +0300

    dark mode

--- imago_project/imago_project/urls.py
+++ imago_project/imago_project/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from imago import views
 
@@ -23,4 +25,4 @@ urlpatterns = [
     path('', include('imago.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('signup/', views.SignUpView.as_view(), name='signup')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
