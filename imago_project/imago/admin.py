f2f8c10a29be6cbf7a0cd88068c4fc009b955562
Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2020-10-09 10:26:22 +0300

    update admin

--- imago_project/imago/admin.py
+++ imago_project/imago/admin.py
@@ -2,32 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-from photologue.admin import GalleryAdmin as GalleryAdminDefault
-from photologue.admin import PhotoAdmin as PhotoAdminDefault
-from photologue.models import Gallery, Photo
-
-from .models import Play, Member, Award, Venue, PhotoExtended, GalleryExtended
-
-
-
-class PhotoExtendedInline(admin.StackedInline):
-    model = PhotoExtended
-    can_delete = False
-
-class PhotoAdmin(PhotoAdminDefault):
-    """Define our new one-to-one model as an inline of Photologue's Gallery model."""
-
-    inlines = [PhotoExtendedInline, ]
-
-class GalleryExtendedInline(admin.StackedInline):
-    model = GalleryExtended
-    can_delete = False
-
-class GalleryAdmin(GalleryAdminDefault):
-
-    """Define our new one-to-one model as an inline of Photologue's Gallery model."""
-
-    inlines = [GalleryExtendedInline, ]
+from .models import Play, Member, Award, Venue
 
 
 class VenueAndAwardAdmin(admin.ModelAdmin):
@@ -47,7 +22,3 @@ admin.site.register(Play, PlayAdmin)
 admin.site.register(Member, MemberAdmin)
 admin.site.register(Venue, VenueAndAwardAdmin)
 admin.site.register(Award, VenueAndAwardAdmin)
-admin.site.unregister(Gallery)
-admin.site.register(Gallery, GalleryAdmin)
-admin.site.unregister(Photo)
-admin.site.register(Photo, PhotoAdmin)
